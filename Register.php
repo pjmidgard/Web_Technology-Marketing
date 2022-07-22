3be8ebe01c78ae79546415619e2c24c4022e44fb
Java Software <pj.midgard@yahoo.com>
2022-07-22 18:28:01 +0100

    Update Register.php

--- Register.php
+++ Register.php
@@ -253,11 +253,6 @@ echo '
                 $result = mysql_query("SELECT FROM Students WHERE student_id = '$varN1%'");
 				if (!mysql_num_rows($result) && strlen($varN1)>0 && strlen($varN2)>0 && strlen($varN3)>0 && $dlin4>0 && $dlin5>0 && $dlin6>0 && $varN2==$varN3 && $dlin1<=10 && s_numeric($varN1))
 				{    
-					 
-
-					
-					$US4i=$row[0];
-					$_SESSION["login"]="$US4i";
 					$varN3f = password_hash($varN3, PASSWORD_DEFAULT);
 					$sql = "insert into Students (student_id,first_name,last_name,email,password,admin) values('$varN1','$varN4','$varN5','$varN6','$varN3f',0)";
 					$items = $dbConnection->prepare("$sql");
